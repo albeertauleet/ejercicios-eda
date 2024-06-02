59d12647d0770685649991d08f0405f6e4b859d5
Albert Aulet Niubó <101295021+albeertauleet@users.noreply.github.com>
2024-06-02 11:53:51 +0200

    Add files via upload

---
+++ p13994.cc
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <limits>
+
+using namespace std;
+
+typedef pair<double, int> ArcP;
+typedef vector<vector<ArcP>> Graph;
+
+int const infinit = numeric_limits<int>::max();
+
+void entrar_graf (Graph& g, int n, int m)
+{
+    g = Graph(n);
+    int x, y;
+    int p;
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> x >> y >> p;
+        g[x].push_back(make_pair(p, y));
+    }
+
+}
+
+
+
+void dijkstra(const Graph& g, int x, int y, vector<int>& d , vector<int>& p, int& dis)
+{
+    int n = g.size();
+    dis = -1;
+    d = vector<int>(n, infinit);
+    d[x] = 0;
+    p = vector<int>(n, -1);
+    vector<bool> S (n, false);
+    priority_queue<ArcP, vector<ArcP>, greater<ArcP>> Q;
+    Q.push(ArcP(0, x));
+    
+    while (not Q.empty())
+    {
+        int u = Q.top().second;
+        Q.pop();
+        if (not S[u])
+        {
+            S[u] = true;
+            if (u == y)
+            {
+                
+                dis = d[u];
+                if (dis != -1 )
+                {
+                    stack<int> cami;
+                    int pos = y;
+                    while (pos != -1)
+                    {
+                        cami.push(pos);
+                        pos = p[pos];
+                    }
+                    bool primer = true;
+                    while (not cami.empty())
+                    {
+                        if (primer)
+                        {
+                            cout << cami.top();
+                            cami.pop();
+                            primer = false;
+                        }
+                        else 
+                        {
+                            cout << " " << cami.top();
+                            cami.pop();
+                        }
+                    }
+                    cout << endl;
+                }
+                
+            }   
+            int m = g[u].size();
+            for (int i = 0; i < m; ++i) 
+            {
+                int v = g[u][i].second;
+                int c = g[u][i].first;
+                if (d[v] > d[u] + c)
+                {
+                    d[v] = d[u] + c;
+                    p[v] = u;
+                    Q.push(ArcP(d[v], v));
+                    
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, m;
+    while (cin >> n >> m)
+    {
+        Graph g;
+        entrar_graf(g, n, m);
+        vector<int> d;
+        vector<int> p;
+        int x, y;
+        cin >> x >> y;
+        int dis;
+        dijkstra(g, x, y, d, p, dis);
+        if (dis == -1) cout << "no path from " << x << " to " << y << endl;
+
+    }
+}
