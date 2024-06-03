9f21e7a93def27b46c255717ec784485104f88c6
Albert Aulet Niubó <101295021+albeertauleet@users.noreply.github.com>
2024-06-03 18:06:19 +0200

    Add files via upload

---
+++ exhaustive search/p17921.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+vector<bool> files;
+vector<bool> columnes;
+vector<bool> d1;
+vector<bool> d2;
+vector<vector<char>> tauler;
+
+void print_tauler()
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int u = 0; u < n; ++u)
+        {
+            cout << tauler[i][u];
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void colocar_reines(int f)
+{
+    if (f >= n)
+    {
+        print_tauler();
+    }
+    else
+    {
+        for (int c = 0; c < n; ++c)
+        {
+            if (not files[f] and not columnes[c] and not d1[f+c] and not d2[n-1+c-f])
+            {
+                files[f] = true;
+                columnes[c] = true;
+                d1[f+c] = true;
+                d2[n-1+c-f] = true;
+                tauler[f][c] = 'Q';
+                colocar_reines(f+1);
+                tauler[f][c] = '.';
+                files[f] = false;
+                columnes[c] = false;
+                d1[f+c] = false;
+                d2[n-1+c-f] = false;
+
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin >> n;
+    files = vector<bool>(n, false);
+    columnes = vector<bool>(n, false);
+    d1 = vector<bool>(n*2 - 1, false);
+    d2 = vector<bool> (n*2 - 1, false);
+    tauler = vector<vector<char>>(n, vector<char>(n, '.'));
+    colocar_reines(0);
+
+}
